c54853ad35cc1a5f7891ca98790380d261182f6c
KongGal <admin@digfordiamonds.com>
2016-08-28 08:35:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -22,15 +22,15 @@ app.get('/', (req, res) => {
 	res.status(200).send('TestBot');
 });
 
-app.get('/webhook', (req, res) => {
-	if (req.query['hub.mode'] === 'subscribe' &&
--			req.query['hub.verify_token'] === 'VALIDATION_TOKEN') {
-		console.log('validating webhook');
-		res.status(200).send(req.query['hub.challenge']);
-	} else {
-		console.log('failed validation.');
-		res.sendStatus(403).send('Piss off');
-	}
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
 });
 
 app.post('/webhook', (req, res) => {
